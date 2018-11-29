ea695993d5fe6c24719aff710b0e8031654b37d9
Faisal-Ahmad <43377742+Faisal-Ahmad@users.noreply.github.com>
2018-11-29 11:30:44 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	</head>
+	<body>
+	<div class="header">
+		<div id="headbar">
+		<span>Students</span>
+		<span>Staff</span>
+		<span>Almuni</span>
+		<span>myHub</span>
+		</div>
+		<div class="search">
+			<input type="text" placeholder="Search Our Site" size="35px"/>
+			<i class="fas fa-search"></i>
+		</div>
+	</div>
+	<div class="menubar">
+		<div id="logo">
+			<span><b>Bournemouth <br/> University</b></span>
+		</div>
+		<div id="menu">
+		<span><b>Home</b></span>
+		<span><b>Why BU?</b></span>
+		<span><b>Study</b></span>
+		<span id="select"><b>Research</b></span>
+		<span><b>Collaborate</b></span>
+		<span><b>Global BU</b></span>
+		<span><b>About</b></span>
+		</div>
+	</div>
+	<div class="mainimage">
+	<div class="showselected">
+		<p>Themes  | Centres  | Environment  | Postgraduate  | Our latest research  |Research Chromicle  |<br/> 
+		British conference of Ingraduate Research | Showcasing Undergraduate Research Excellence
+		</p>
+	</div>
+	<div class="imagecontent">
+		<span><b>World-leading <br/> research</span></b>
+		<br/><br/><br/>
+		<span id="trasparent">Read the latest Bournemouth Reasearch Chonicle 2018</span>
+	</div>
+	<span id="vertical">Leave a message</span>
+	</div>
+	<div class="maincontent">
+		<div class="content">
+			<input type="text" placeholder="Search Our Courses" size="128px"/>
+			<i class="fas fa-search"></i>
+		</div>
+		<div class="black">
+		</div>
+		<div class="contexttxt">
+		<p><span>Home</span>/Research</p>
+		<hr/>
+		<p>Our Resesrch and changes the world around us, providing solution to real-world problems and informing the education we deliver.<br>
+		out students are key part of the resesrch we conduct,co-creating knowledge with us playing a crucial role in everywher that we do.</p>
+		</div>
+	</div>
+	<div class="grid">
+		<div class="sub1">
+			<div class="grid1">
+				<h2>Research themes</h2>
+				<div class="white">
+				</div>
+				<span>Find out about this valid reserch we undertake.</span>
+			</div>
+			<div class="grid2">
+				<p>Postgraduate reserch (including phD)</p>
+			</div>
+			<div class="grid3">
+				<h2>Research contres</h2>
+				<div class="white">
+				</div>
+				<span>Learn more about multi-disciplinary research center.</span>
+			</div>
+		</div>
+		<div class="sub2">
+			<div class="grid4">
+				<p>Resecrch impact</p>
+			</div>
+			<div class="grid5">
+				<p>Research Events</p>
+			</div>
+		</div>
+		<div class="sub3">
+			<div class="grid6">
+				<p>Research news</p>
+			</div>
+			<div class="grid7">
+				<p>Reserch briefings</p>
+			</div>
+		</div>
+		<div class="black2">
+		<div>
+	</div>
+	
+	</body>
+</html>
